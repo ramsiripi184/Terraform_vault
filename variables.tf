6b45dd606db60d4f29f8dc8303ae2ee1621bf2c6
SIRIPI VENKATARAMESWARA REDDY <54143244+ramsiripi184@users.noreply.github.com>
2023-03-02 20:17:01 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "vault_addr" {
-  default="https://vault.kyndryl.net/"
-  #default="http://127.0.0.1:8200"
+  #default="https://vault.kyndryl.net/"
+  default="http://127.0.0.1:8200"
 }
 variable "vault_token" {
-  default = "hvs.CAESIJ4Uuq23Vq-1r9k7uYOv352bpMeB8ovw1-S8EL_MYuyfGikKImh2cy5LRnhhUEM0UjFxaUl6bWlDbUlyaGR2cVcuQVl6ZmQQnPG2VQ"
- # default = "root01~"
+ # default = "hvs-1r9k7uYOv352bpMeB8ovw1-S8EL_MYuyfGikKImh2cy5LRnhhUEM0UjFxaUl6bWlDbUlyaGR2cVcuQVl6ZmQQnPG2VQ"
+  default = "root"
 }
 
 variable "vault_namespace" {
@@ -27,14 +27,39 @@ variable "vault_entity"{
   default = "Siripi.Venkatarameswara.Reddy@kyndryl.com"
 }
 
-#key values
-variable "appid"{
-  default = "1230987654"
+variable "second_entity"{
+  default = "venkat"
 }
 
-variable "secretid"{
-  default = "12345678"
+#key values
+variable "vmbuildid"{
+  default = "876435645675678854"
+}
+
+variable "clientid"{
+  default = "8764356464354"
 }
 variable "tenantid"{
-  default = "87654"
+  default = "8763245678654354"
+}
+variable "secretid"{
+  default = "8754543534654"
+}
+variable "subscriptionid"{
+  default = "87324567865435674567654"
+}
+variable "pullsecretid"{
+  default = "87565654332654"
+}
+variable "tokenid"{
+  default = "8765678765454"
+}
+variable "objectid"{
+  default = "87634554"
+}
+variable "aadid"{
+  default = "876543245654"
+}
+variable "aadsecretid"{
+  default = "8765432456541"
 }
